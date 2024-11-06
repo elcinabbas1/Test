41f67964ee96fed5713749b3e79d4e13b7270d9b
elcinabbas1 <goychay1993@gmail.com>
2024-11-06 09:37:51 +0400

    "Action-2"

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Salam Baki");
+        if(true){
+            System.out.println("Abbasi");
+        }
     }
 }
